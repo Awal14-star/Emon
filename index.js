2fae583dfea516761638a6cd16a91e672a5e50b5
Keito-Klein <77515527+Keito-Klein@users.noreply.github.com>
2021-01-26 00:22:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ const { removeBackgroundFromImageFile } = require('remove.bg')
 const { ind } = require('./language')
 const vcard = 'BEGIN:VCARD\n' 
             + 'VERSION:3.0\n' 
-            + 'FN:Affis Admin\n' 
-            + 'ORG: Pengembang XBot;\n' 
-            + 'TEL;type=CELL;type=VOICE;waid=6282334297175:+62 823-3429-7175\n' 
+            + 'FN:MiKako Admin\n' 
+            + 'ORG: Pengembang Botol-LoL;\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=6289675651966:+62 896-7565-1966\n' 
             + 'END:VCARD' 
 prefix = '#'
 blocked = []   
