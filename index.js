1f8ead4c7885e16df541e4c32578ffe763fc169a
Keito-Klein <77515527+Keito-Klein@users.noreply.github.com>
2021-01-27 12:09:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ limitawal = '20'
 cr = '*BOT INI SUDAH TERVERIFIKASI*'
 
 /******** OWNER NUMBER**********/
-const ownerNumber = ["62895710073737@s.whatsapp.net","6282334297174@s.whatsapp.net"] 
+const ownerNumber = ["6289675651966@s.whatsapp.net","6281231958474@s.whatsapp.net"] 
 /************************************/
 
        
