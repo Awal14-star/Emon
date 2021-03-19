caf022f1483630829be5d0a12254056111941f7c
MiKako <77515527+Keito-Klein@users.noreply.github.com>
2021-03-20 04:58:54 +0700

    Delete double lolis life

--- install.sh
+++ install.sh
@@ -6,9 +6,7 @@ apt-get install nodejs -y
 apt-get install libwebp -y
 npm i imgbb-uploader -y
 npm i lolis.life -y
-npm i lolis.life
 npm i @kagchi/kag-api
-npm install -g npm@7.6.1
 apt-get install mc -y
 apt-get install ffmpeg -y
 apt-get install wget -y
